cd829dfe8ea1c03928c93f7f88c687cfa31df81d
toshi0383 <t.suzuki326@gmail.com>
2018-07-30 20:21:57 +0900

    Update bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION=${1:?}
 CURRENT=${2:-`hackscode --version`}
-sed -i "" -e "s/master/${VERSION}/" CHANGELOG.md
+#sed -i "" -e "s/master/${VERSION}/" CHANGELOG.md
 git grep -l $CURRENT | grep -v CHANGELOG.md | xargs sed -i "" -e "s/${CURRENT}/${VERSION}/g"
