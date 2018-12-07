499dad2ca74a8789d0a5afb1a602e08de844468c
toshi0383 <t.suzuki326@gmail.com>
2018-12-07 13:49:32 +0900

    Use forked xcodeproj

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "hackscode",
     dependencies: [
-        .package(url: "https://github.com/tuist/xcodeproj", .branch("master")),
+        .package(url: "https://github.com/toshi0383/xcodeproj", .branch("instruments-package")),
         .package(url: "https://github.com/toshi0383/CoreCLI", from: "0.1.10"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
     ],
