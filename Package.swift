84166741b6c109f9fdb08e888f75d8aac80acbe2
toshi0383 <t.suzuki326@gmail.com>
2018-12-07 13:12:21 +0900

    Update tuist URL

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "hackscode",
     dependencies: [
-        .package(url: "https://github.com/xcode-project-manager/xcodeproj", .branchItem("master")),
+        .package(url: "https://github.com/tuist/xcodeproj", .branchItem("master")),
         .package(url: "https://github.com/toshi0383/CoreCLI", from: "0.1.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
     ],
